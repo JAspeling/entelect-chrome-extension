5cf9c8370afda82ff48723f5bea72df9a044c161
Johan Aspeling <Johan.Aspeling@absa.africa>
2020-04-28 12:54:47 +0200

    initial commit

---
+++ entelect/js/dismiss-notifications.js
@@ -0,0 +1,31 @@
+console.log('Dismissing notifications')
+
+exclusions = [];
+
+var containsOneOf = function (value, strings, ignoreCase = true) {
+    let target = value;
+
+    target = ignoreCase ? target.toLocaleUpperCase() : target;
+    if (!strings || strings.length === 0) {
+        return false;
+    }
+
+    strings = strings.map((str) => {
+        return ignoreCase ? str.toLocaleUpperCase() : str;
+    });
+    for (const str of strings) {
+        if (target.includes(str)) {
+            return true;
+        }
+    }
+
+    return false;
+};
+chrome.storage.sync.get('notificationExclusions', function (data) {
+    exclusions = data.notificationExclusions;
+    console.log('Excluding', exclusions);
+
+    [...document.querySelectorAll('.alert-notification')]
+        .filter(n => !containsOneOf(n.childNodes[1].innerText, exclusions, true))
+        .forEach(n => n.childNodes[0].click());
+});
---
+++ entelect/js/options.js
@@ -0,0 +1,76 @@
+function isNullOrUndefined(value) {
+    return value == null || value == undefined;
+}
+
+let exclusions = document.getElementById('exclusions');
+let addExclusionButton = document.getElementById('addExclusion');
+
+const saveExclusions = document.getElementById('saveExclusions');
+
+saveExclusions.onclick = function () {
+    saveOptions();
+
+    // chrome.storage.sync.set({ "notificationExclusions": data }, function () {
+    //     console.log('Saved exclusions as ' + data);
+    // })
+}
+
+function addExclusion(exclusion) {
+    let itemContainer = document.createElement("div");
+    itemContainer.classList.add("form-group");
+    itemContainer.classList.add("row");
+
+    let inputContainer = document.createElement("div");
+    inputContainer.classList.add("col-11");
+
+    let inputElement = document.createElement("input");
+    inputElement.classList.add("form-control");
+    inputElement.value = exclusion;
+
+    inputContainer.appendChild(inputElement);
+
+    let removeElement = document.createElement("button");
+    removeElement.classList.add("btn");
+    removeElement.classList.add("btn-danger");
+    removeElement.classList.add("remove");
+    removeElement.textContent = "-";
+
+    itemContainer.appendChild(inputContainer);
+    itemContainer.appendChild(removeElement);
+
+    removeElement.addEventListener('click', function () {
+        debugger;
+        exclusions.removeChild(itemContainer);
+    });
+    exclusions.appendChild(itemContainer);
+}
+
+// Function to add the saved options here.
+function getOptions() {
+    chrome.storage.sync.get('notificationExclusions', function (data) {
+        const value = data.notificationExclusions;
+        console.log('chrome.storage.sync.get', value);
+
+        if (!isNullOrUndefined(value) && value.length > 0) {
+            console.log('Value array', value);
+
+            value.forEach(val => addExclusion(val));
+        } else {
+            console.log('Values not found.', value);
+        }
+    })
+}
+
+function saveOptions() {
+    // Save the Value items 
+    var inputs = [...exclusions.querySelectorAll('input')].map(input => input.value);
+    console.log(inputs);
+
+    chrome.storage.sync.set({ 'notificationExclusions': inputs });
+}
+
+getOptions();
+
+addExclusionButton.onclick = function () {
+    addExclusion("");
+}
