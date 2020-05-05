2cade03808b30c996fae7e786ee0da5fe04a8b0d
Johan Aspeling <Johan.Aspeling@absa.africa>
2020-05-05 17:51:11 +0200

    chore started with basic routing

--- web-server/src/routing/link-repo.js
+++ web-server/src/routing/link-repo.js
@@ -4,6 +4,20 @@ const route = (app) => {
     app.get('/link-repo', (reg, res) => {
         res.send(["value1", "value2"])
     });
+
+    app.post('/link-repo', (req, res) => {
+        const linkQuery = req.query.link
+        if (!linkQuery) {
+            return res.send({
+                error: 'Invalid url! Link not provided'
+            });
+        }
+
+
+        console.log('Got body:', req.body);
+        res.sendStatus(200);
+    });
+
 };
 
 module.exports = { route };
