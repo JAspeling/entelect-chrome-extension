ae9ec382973a9528f2b7cfc732ee3e719b9f3962
Johan Aspeling <Johan.Aspeling@absa.africa>
2020-05-16 15:48:23 +0200

    chore: WIP: added webpack, started building the output.

---
+++ entelect/webpack.config.js
@@ -0,0 +1,97 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const baseManifest = require("./chrome/manifest.json");
+const WebpackExtensionManifestPlugin = require("webpack-extension-manifest-plugin");
+
+const config = {
+    mode: 'development',
+    entry: {
+        popup: './src/popup/popup.ts',
+        options: './src/options/options.ts',
+        styles: './src/scss/styles.scss'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: 'dist' // required for webpack dev server, not needed here.
+    },
+    devtool: 'inline-source-map',
+    module: {
+        // loader is a package that tells webpack how to deal with certain files.
+        rules: [
+            {
+                //whether the rule applies to this file or not
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'assets/css/[name].css',
+                        }
+                    },
+                    {
+                        loader: 'extract-loader'
+                    },
+                    {
+                        loader: 'css-loader?-url'
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ],
+            },
+        ]
+    },
+    plugins: [
+        // new HtmlWebpackPlugin({
+        //     title: "Entelect Extensions",
+        //     meta: {
+        //         charset: "utf-8",
+        //         viewport: "width=device-width, initial-scale=1, shrink-to-fit=no", "theme-color": "#000000"
+        //     },
+        //     manifest: "manifest.json",
+        //     filename: "index.html",
+        //     template: "./static/index.html",
+        //     hash: true
+        // }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/popup/popup.html',
+            chunks: ['popup']
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'options.html',
+            template: 'src/options/options.html',
+            chunks: ['options']
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: 'chrome/icons', to: 'assets/icons' },
+                { from: 'vendor', to: 'assets/vendor' },
+            ],
+        }),
+        new WebpackExtensionManifestPlugin({
+            config: {
+                base: baseManifest
+            }
+        })
+    ],
+    // which file extensions it adds to the imports it finds
+    resolve: {
+        extensions: [
+            '.ts', '.js'
+        ]
+    }
+};
+
+module.exports = config;
