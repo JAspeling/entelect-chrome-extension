e8ffd9405882a3f663ec121b91b7d5cd302864a4
Johan Aspeling <Johan.Aspeling@absa.africa>
2020-05-25 17:13:45 +0200

    chore: added production link

--- entelect-ext/src/environments/environment.prod.ts
+++ entelect-ext/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { Environment } from './environment-class';
 
 export const environment = new Environment(
     new EnvironmentState({
-        production: false,
-        api: 'http://localhost:27017/'
+        production: true,
+        api: 'https://entelect-extension-webserver.herokuapp.com/'
     })
 );
